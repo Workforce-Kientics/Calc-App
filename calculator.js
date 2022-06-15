981c712c98409af7d3418ce4f310f3a5ce706899
Workforce-Kinetics <support@workforcekinetics.com>
2022-06-15 12:18:37 -0500

    Final commit

--- calculator.js
+++ calculator.js
@@ -1,37 +1,31 @@
 
- function calcSalary() {
-     var annualSal= parseFloat(document.getElementById('annwage').value);
-     var hours=parseFloat(document.getElementById('txt_hours').value);
-     var monSal= parseFloat(document.getElementById('monwage').value);
-     var fortSal=parseFloat(document.getElementById('fwage').value);
-     var wkSal=parseFloat(document.getElementById('wkwage').value);
-     var daySal=parseFloat(document.getElementById('dwage').value);
-     var hourSal=parseFloat(document.getElementById('hourwage').value);
-     var calculate= 0
 
-     if( annualSal== 0 && monSal==0 && fortSal==0 && wkSal==0 && daySal==0 && hourSal!=0) {
-         calculate= hourSal * hours;
-     }
-     if( annualSal==0 && monSal==0 && fortSal==0 && wkSal==0 && daySal!=0 && hourSal==0){
-         calculate= daySal * hours;
-     }
-     if( annualSal==0 && monSal==0 && fortSal==0 && wkSal !=0 && daySal==0 && hourSal==0){
-         calculate= wkSal * hours;
-     }
-     if(annualSal==0 && monSal==0 && fortSal!=0 && wkSal==0 && daySal==0 && hourSal==0 ){
-         calculate= fortSal * hours;
-     }
-     if(annualSal==0 && monSal!=0 && fortSal==0 && wkSal==0 && daySal==0 && hourSal==0){
-         calculate= monSal * hours;
-     }
-     if(annualSal!=0 && monSal==0 && fortSal==0 && wkSal==0 && daySal==0 && hourSal==0){
-        var calculate= annualSal * hours * 52;
+
+
+var monSal= parseFloat(document.getElementById('monwage'));
+var fortSal=parseFloat(document.getElementById('fwage'));
+var wkSal=parseFloat(document.getElementById('wkwage'));
+var daySal=parseFloat(document.getElementById('dwage'));
+var hours=parseFloat(document.getElementById('txt_hours'));
+var hourSal=parseFloat(document.getElementById('hourwage'));
+var CalcButton= document.getElementById('calc');
+
+CalcButton.addEventListener("click", annualSalary());
+
+
+function annualSalary(){
+
+    var hourlyRate= hourSal;
+    var hours_worked= hours;
+    var Salary= hourlyRate * hours_worked * 52;
+    document.getElementById('results').innerHTML = Salary;
+}
+    
+
      
+    
+    
      
-     
-     }
-     document.getElementById('results').innerHTML= calculate;
- }
 
  
 
